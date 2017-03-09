f3afdafe3729505fed1b7ea06c632ad86322ad18
395123822@qq.com <395123822@qq.com>
2017-03-09 18:24:03 +0800

    test6

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
 // PRELOADER
 
 $(window).load(function(){
-    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+    $('.preloader').delay(500).fadeOut("slow"); // set duration in brackets
 });
 
 // HOME BACKGROUND SLIDESHOW
